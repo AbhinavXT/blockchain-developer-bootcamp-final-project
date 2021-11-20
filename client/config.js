db413f27196fcaa7865e3e40e53d6f5cf2680f67
Abhinav Pathak <56548922+AbhinavXT@users.noreply.github.com>
2021-11-21 02:25:52 +0530

    Update config.js

--- client/config.js
+++ client/config.js
@@ -1,4 +1,5 @@
-export const nftContractAddress = '0xf2fdbaaE48E16c28115D75d6e5642E8Bdee27997'
+
 export const nftMarketAddress = '0xF8cC13b9ff3999E88Ef2B4635D1fB536A87c4059'
+export const nftContractAddress = '0xf2fdbaaE48E16c28115D75d6e5642E8Bdee27997'
 
 export default { nftMarketAddress, nftContractAddress }
